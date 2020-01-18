9dea9f2a1c450f76be695a9d7c1e89b5127256aa
Kamil Sawicki <sawicki_kamil@onet.pl>
2020-01-18 22:24:33 +0100

    implemented map service, and refactored other services

--- pet-clinic-data/src/main/java/petClinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/petClinic/services/OwnerService.java
@@ -2,8 +2,6 @@ package petClinic.services;
 
 import petClinic.model.Owner;
 
-import java.util.Set;
-
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByName(String lastName);
 
