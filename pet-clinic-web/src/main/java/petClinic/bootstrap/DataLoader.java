db4a4471504b083159df97b55e85395cc3342b18
Kamil Sawicki <sawicki_kamil@onet.pl>
2020-01-19 23:15:31 +0100

    Refactored AbstractMapService ID to auto implementation

--- pet-clinic-web/src/main/java/petClinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/petClinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Jacek");
         owner1.setLastName("Malysiak");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Mojsze");
         owner2.setLastName("Izrael");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loading owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Marta");
         vet1.setLastName("Mazurek");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Kasia");
         vet2.setLastName("Mila");
 
