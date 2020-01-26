9c093316c636b0fd9947be052e280e90d8ce3836
Kamil Sawicki <sawicki_kamil@onet.pl>
2020-01-26 14:30:21 +0100

    Set up junit 5 dependencies to mvn

--- pet-clinic-web/src/test/java/petClinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petClinic/PetClinicApplicationTests.java
@@ -1,12 +1,14 @@
 package petClinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class PetClinicApplicationTests {
-
- //   @Test
+    @Test
     void contextLoads() {
     }
 
